78fbf2d442a63eb22e32ab3171a0e76ef06666a8
TAEYEON KIM <jenniferkim0704@outlook.kr>
2026-03-19 12:25:44 +0900

    과제1 코딩과 테스트 완료 UI 구성, 전송기능, 입력창 정리

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblTitle = new Label();
+            txtMessenger = new TextBox();
+            lstEchoWindow = new ListBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("MV Boli", 30F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = Color.Blue;
+            lblTitle.Location = new Point(12, 9);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(406, 65);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Echo Messenger";
+            // 
+            // txtMessenger
+            // 
+            txtMessenger.Font = new Font("맑은 고딕", 15F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtMessenger.Location = new Point(12, 370);
+            txtMessenger.Name = "txtMessenger";
+            txtMessenger.Size = new Size(542, 41);
+            txtMessenger.TabIndex = 1;
+            txtMessenger.Text = "(여기에 입력하세요)";
+            // 
+            // lstEchoWindow
+            // 
+            lstEchoWindow.BackColor = SystemColors.HighlightText;
+            lstEchoWindow.Font = new Font("맑은 고딕", 15F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lstEchoWindow.FormattingEnabled = true;
+            lstEchoWindow.Location = new Point(12, 79);
+            lstEchoWindow.Name = "lstEchoWindow";
+            lstEchoWindow.Size = new Size(738, 249);
+            lstEchoWindow.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = SystemColors.ButtonHighlight;
+            btnSend.Font = new Font("바탕", 15F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnSend.ForeColor = SystemColors.WindowText;
+            btnSend.Location = new Point(560, 373);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(141, 41);
+            btnSend.TabIndex = 3;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += btnSend_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnSend);
+            Controls.Add(lstEchoWindow);
+            Controls.Add(txtMessenger);
+            Controls.Add(lblTitle);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTitle;
+        private TextBox txtMessenger;
+        private ListBox lstEchoWindow;
+        private Button btnSend;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,12 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string typed_msg = txtMessenger.Text; // txtMessenger 텍스트박스에 입력된 문자열을 가져와서 typed_msg 변수에 저장
+            lstEchoWindow.Items.Add(typed_msg); // lstEchoWindow 리스트박스에 typed_msg 변수를 추가하여 메시지를 표시
+            txtMessenger.Clear(); // txtMessenger 텍스트박스의 내용을 지워서 다음 메시지를 입력할 수 있도록 준비
+        }
     }
 }
